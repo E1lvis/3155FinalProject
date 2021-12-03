1ef17ada61e3f7bbccf8bc4ac4d27b7675426b00
msolom15 <89551729+INFRAsound123@users.noreply.github.com>
2021-12-03 02:53:08 -0500

    Update Waste percentage by country.py

--- Code/Waste percentage by country.py
+++ Code/Waste percentage by country.py
@@ -8,7 +8,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-df = pd.read_csv("Datasets\Country level dataset.csv")
+df = pd.read_csv("Final_Website/Datasets/Country level dataset.csv")
 
 app = dash.Dash(__name__)
 
